ef6059ad46e91b8fb9f695e1b48915c781cfdf78
Gerard Choinka <gerard.choinka@gmail.com>
2023-04-09 03:16:54 +0100

    tolerance adjustment
    reduced screew hole size, made screws go 1 mm deaper
    removed slim feet, add break point to convert a wide feet to slim feet
    change the slopes

--- src/feet.py
+++ src/feet.py
@@ -5,8 +5,6 @@ from typing import List, Any
 from solid2 import *
 from math import sin, pi
 
-from solid2 import OpenSCADConstant
-
 
 def square_with_cut_corners(width, depth, cutoff_length):
     a = sin(pi / 4) / sin(pi / 2) * cutoff_length
@@ -17,32 +15,27 @@ def square_with_cut_corners(width, depth, cutoff_length):
 
 
 def screw_place():
-    h = 2.5
-    r = 12.5 / 2
-    return (cylinder(r2=12.5 / 2, r1=r * 1.25, h=h, center=True) + cylinder(r=4 / 2, h=30, center=True, _fn=20)).up(
-        h / 2 - 0.1)
+    h = 3.5
+    washer_r = 12.5 / 2
+    screw_r = 3 / 2
+    return (cylinder(r2=washer_r, r1=washer_r * 1.25, h=h, center=True) + cylinder(r=screw_r, h=30, center=True,
+                                                                                   _fn=20)).up(h / 2 - 0.1)
 
 
 def flap_raw(feet_width, edge_depth, inside_depth, flap_width, flap_height, feet_height):
-    p = [(feet_width / 2 - edge_depth, -feet_width / 2),
-         (feet_width / 2, -feet_width / 2),
-         (feet_width / 2, -feet_width / 2 + flap_width + 3),
-         (feet_width / 2 - 3, -feet_width / 2 + flap_width),
+    p = [(feet_width / 2 - edge_depth, -feet_width / 2), (feet_width / 2, -feet_width / 2),
+         (feet_width / 2, -feet_width / 2 + flap_width + 3), (feet_width / 2 - 3, -feet_width / 2 + flap_width),
          (feet_width / 2 - inside_depth, -feet_width / 2 + flap_width)]
     s = linear_extrude(flap_height + 0.05)(polygon(points=p))
     base = s.up(feet_height).down(flap_height)
-    front_slope = s.rotate([20, 0, 0]).up(feet_height + 11).translate([0, -3, 0]).down(flap_height / 2 + 0.25)
-    side_slope = s.rotate([0, 5, 0]).up(feet_height + 0.5).left(-0.1).down(flap_height / 2)
+    side_slope = s.rotate([20, 0, 0]).up(feet_height + 11).translate([0, -3, 0]).down(flap_height / 2 + 1)
+    front_slope = s.rotate([0, 5, 0]).up(feet_height + 0.5).left(-0.1).down(flap_height / 1.5)
     return base + front_slope + side_slope
 
 
 # TODO: remove feet_height, add front_slope and side_slope and replace flap_raw
 def flap_raw_wip(edge_depth, inside_depth, flap_width, flap_height, feet_height):
-    p = [(- edge_depth, 0),
-         (0, 0),
-         (0, flap_width + 3),
-         (- 3, flap_width),
-         (- inside_depth, flap_width)]
+    p = [(- edge_depth, 0), (0, 0), (0, flap_width + 3), (- 3, flap_width), (- inside_depth, flap_width)]
     s = linear_extrude(flap_height + 0.05)(polygon(points=p))
     base = s.up(feet_height).down(flap_height)
     return base
@@ -59,8 +52,8 @@ def feet():
     bottom_depth = bottom_square_xy
     top_corner_cutoff_length = 10
     bottom_corner_cutoff_length = 6
-    flap_height = 4
-    flap_width = 13
+    flap_height = 4.5
+    flap_width = 13.75
     flap_edge_depth = 37
     flap_inside_depth = 25
     depth_reduction = 74
@@ -79,16 +72,18 @@ def feet():
     screws['middle'] = screw_place().translate([9, top_width / 2 - 13 - 0.5, 0])
     screws['back'] = screw_place().translate([17.5, top_width / 2 - 13 - 10, 0])
 
+
     all_screws = screws['front'] + screws['middle'] + screws['back']
+
+
 
     feet_wide = feet_mold - flap - flap.mirrorY() - all_screws - all_screws.mirrorY()
 
-    feet_slim = feet_wide.translate([0, top_width / 2 - 13 - 10, 0]) - cube([top_width, top_width, feet_height + 0.1]).down(
-        0.05).left(5)
+    breaking_point_bottom = cube([bottom_depth/2, 21,0.1], center=True).right(bottom_width/4)
+    breaking_point_top = cube([top_depth / 2+10, 0.1,0.1], center=True).right(bottom_width / 4).up(feet_height-1)
 
-    feet_slim = feet_slim + feet_slim.mirrorY()
-
-    return [(feet_wide, "feet"), (feet_slim, "feet_slim")]
+    breaking_point = hull()(breaking_point_top , breaking_point_bottom)
+    return [(feet_wide-breaking_point, "feet")]
 
 
 def main(output_scad_basename, output_stl_basename):
